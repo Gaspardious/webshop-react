825ec786bbfc6f9769482e9a959b6e4aeaef7b54
Dejan Gaspar <107142760+Gaspardious@users.noreply.github.com>
2024-04-14 21:45:47 +0200

    Add files via upload

--- webshop-react/src/components/card.jsx
+++ webshop-react/src/components/card.jsx
@@ -3,16 +3,19 @@ export default function Card() {
 
     return (
         <div className="card">
-            <img src="./src/sweaters/rosa-fram.jpeg" alt="pink sweater" className="sweaters" />
-            <div className="product__info">
-                <h1 className="product__name">Mr.Pink</h1>
-                <h2 className="product__price">249 SEK</h2>
-                    <div className="product__size">
-                        <p><small>SIZE</small></p>
-                        <p><strong>L</strong></p>
-                    </div>
-                    
+            <div className="product__section">
+                <img src="./src/sweaters/rosa-fram.jpeg" alt="pink sweater" className="sweaters" />
+                <div className="product__size">
+                                <p><strong>SIZE</strong></p>
+                                <p><small>M / L / XL</small></p>
+                </div>
             </div>
+
+            <h1 className="product__company">STICKY SWEATER</h1>
+                <div className="product__info">
+                    <h1 className="product__name">GLENN</h1>
+                    <h2 className="product__price">249 SEK</h2>      
+                </div>
             <button className="product__button">BUY</button>
         </div>
     );
