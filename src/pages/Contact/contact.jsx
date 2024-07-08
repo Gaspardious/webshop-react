b6b99ef28a8f2d25ef3c0c3af52cc57b3c820ecd
Dejan Gaspar <107142760+Gaspardious@users.noreply.github.com>
2024-07-08 19:26:22 +0200

    css modules added to contact

--- src/pages/Contact/contact.jsx
+++ src/pages/Contact/contact.jsx
@@ -1,15 +1,14 @@
-import './contact.css'
-
+import styles from './contact.module.css'
 
 
 export default function Contact () {
     return (
     <>
-        <div className='contact'>
-            <section className='contact__section'>
-                <h1 className='contact__title'>CONTACT</h1>
-                    <p className='contact__desc'>Mail: dejan.gaspar88@gmail.com</p>
-                    <p className='contact__desc'>Phone: 123-456-78</p>
+        <div className={styles.contact}>
+            <section className={styles.contact__section}>
+                <h1 className={styles.contact__title}>CONTACT</h1>
+                    <p className={styles.contact__desc}>Mail: dejan.gaspar88@gmail.com</p>
+                    <p className={styles.contact__desc}>Phone: 123-456-78</p>
             </section>
         </div>
     </>
