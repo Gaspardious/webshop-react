0e1e6fe33224e5523c63070dc4f41586dd851b50
Dejan Gaspar <107142760+Gaspardious@users.noreply.github.com>
2024-07-08 19:21:56 +0200

    css modules added

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,12 +1,12 @@
 
-import './footer.css'
+import styles from './footer.module.css'
 
 export default function Footer (){
 return (
     <>
         <footer>
             <div>
-            <p>© Made by Dejan Gaspar - 2024</p>
+            <p className={styles.footer}>© Made by Dejan Gaspar - 2024</p>
             </div>
            
         </footer>
