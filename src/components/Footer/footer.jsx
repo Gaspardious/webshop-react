5dda9cf8a8beabe86582c94c903ecae14b1e0c35
Dejan Gaspar <107142760+Gaspardious@users.noreply.github.com>
2024-07-12 18:51:03 +0200

    changes made to hero page

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -6,9 +6,9 @@ return (
     <>
         <footer>
             <div>
+            <h2 className={styles.brandname}>STICKY SWEATER</h2>
             <p className={styles.footer}>© Made by Dejan Gaspar - 2024</p>
             </div>
-           
         </footer>
     </>
 )}
--- src/pages/Homepage/homepage.jsx
+++ src/pages/Homepage/homepage.jsx
@@ -9,8 +9,8 @@ export default function Homepage () {
         <div className={styles.hero}>
             <img src="/Images/rosa-fram.jpeg" alt="" className={styles.hero__img} />
             <div className={styles.hero__desc}>
-                <h1>SEE ALL PRODUCTS!</h1>
-                <h3>MADE IN SWEDEN!</h3>
+                <h1>STICKY SWEATER</h1>
+                <h3 className={styles.tag}>DESIGNED IN SWEDEN - MADE IN ITALY</h3>
                 <li className={styles.hero__btn}><Link to="/products">GO TO PRODUCTS</Link></li>
             </div>
         </div>
