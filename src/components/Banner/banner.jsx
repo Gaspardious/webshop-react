c766a6dfb51d97d3431d51a57c2ddea10067d5b6
Dejan Gaspar <107142760+Gaspardious@users.noreply.github.com>
2024-07-15 18:55:08 +0200

    fixed header added

--- src/components/Banner/banner.jsx
+++ src/components/Banner/banner.jsx
@@ -2,9 +2,9 @@ import styles from './banner.module.css'
 
 const Banner = () => {
   return (
-    <>
+    <div className={styles.banner_section}>
     <p className={styles.banner}>DESIGNED IN SWEDEN - MADE IN ITALY</p>
-    </>
+    </div>
   )
 }
 
--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,21 +1,46 @@
-import { Link } from 'react-router-dom'
-import styles from './header.module.css'
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import styles from './header.module.css';
 
-export default function Header (){
-    return (
-        <>
-            <header>
-            <Link to="/">
-                <img src="/Images/logo.png" alt="" className={styles.logo} />
-                </Link>
-                    <nav>
-                        <ul>
-                            <li><Link to="/">HOME</Link></li>
-                            <li><Link to="/products">PRODUCTS</Link></li>
-                            <li><Link to="/about">ABOUT</Link></li>
-                            <li><Link to="/contact">CONTACT</Link></li>
-                        </ul>
-                    </nav>
-            </header>
-        </>
-)}
+export default function Header() {
+  useEffect(() => {
+    function handleScroll() {
+      const header = document.getElementsByClassName(styles.header)[0];
+      const logo = document.getElementsByClassName(styles.logo)[0];
+
+      if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        header.style.height = "70px";
+        logo.style.width = "70px";
+        header.style.background = "rgba(168, 168, 168)";
+      } else {
+        header.style.height = "100px";
+        logo.style.width = "110px";
+        header.style.background = "rgba(205, 205, 205)";
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <>
+      <header className={styles.header}>
+        <Link to="/">
+          <img src="/Images/logo.png" alt="" className={styles.logo} />
+        </Link>
+        <nav>
+          <ul>
+            <li><Link to="/">HOME</Link></li>
+            <li><Link to="/products">PRODUCTS</Link></li>
+            <li><Link to="/about">ABOUT</Link></li>
+            <li><Link to="/contact">CONTACT</Link></li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
