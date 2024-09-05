f94107292b836feb943d51a0bdaf1f8c86706ca3
Dejan Gaspar <107142760+Gaspardious@users.noreply.github.com>
2024-09-05 09:06:29 +0200

    small fix on homepage

--- src/components/Card/card.jsx
+++ src/components/Card/card.jsx
@@ -23,11 +23,6 @@ const Card = ({product}) => {
                 <img src={`../Images/${product.img[0]}`} className={isHovered ? `${styles.sweaters} ${styles.shrink}` : styles.sweaters}         
                 onMouseEnter={mouseEnter} onMouseLeave={mouseLeave} />
             </Link>
-
-                <div className={styles.product__size}>
-                                <p><strong>SIZE</strong></p>
-                                <p><small>M / L / XL</small></p>
-                </div>
             </div>
 
 
@@ -42,9 +37,6 @@ const Card = ({product}) => {
                     </Link> 
                     <h2 className={styles.product__price}>{product.price} SEK</h2>      
                 </div>
-     {/*            <Link to={`/product/${product.id}`}>
-                    <button className={styles.product__button}>ADD TO CART</button>
-                </Link> */}
         </div>
     );
 }
