d33cbc836a154e2463d0e3642ba53f4db77649af
Dejan Gaspar <107142760+Gaspardious@users.noreply.github.com>
2024-09-12 13:48:22 +0200

    changes to checkout

--- src/pages/Checkout/checkout.jsx
+++ src/pages/Checkout/checkout.jsx
@@ -21,8 +21,8 @@ const Checkout = () => {
 
   
     <div className={styles.checkout_summary}>
-              <details>
-                <summary>
+              <details  className={styles.checkout_details_section}>
+                <summary className={styles.checkout_summary_section}>
                   <h3 className={styles.checkout_summary_title}>SHOW ORDER SUMMARY</h3>
                   <p className={styles.checkout_summary_price}>{calculateTotalPrice()} SEK</p>
                 </summary>
